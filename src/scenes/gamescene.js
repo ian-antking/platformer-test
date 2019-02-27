7af7a070a426dee3432461f7c97df47c17a6c722
Ian King <ian.antking@gmail.com>
2019-02-27 19:28:07 +0000

    rendered map

--- src/scenes/gamescene.js
+++ src/scenes/gamescene.js
@@ -10,18 +10,12 @@ export default class GameScene extends Phaser.Scene {
   }
 
   create() {
-    // game world
+    // map
     this.map = this.make.tilemap({ key: 'map' });
     this.groundTiles = this.map.addTilesetImage('tiles');
-    this.groundLayer = this.map.createStaticLayer('World', this.groundTiles, 0, 0);
+    this.groundLayer = this.map.createDynamicLayer('World', this.groundTiles, 0, 0);
     this.groundLayer.setCollisionByExclusion([-1]);
     this.physics.world.bounds.width = this.groundLayer.width;
     this.physics.world.bounds.height = this.groundLayer.height;
-
-    // player
-    this.player = this.physics.add.sprite(200, 200, 'player');
-    this.player.setBounce(0.2);
-    this.player.setCollideWorldBounds(true);
-    this.physics.add.collider(this.groundLayer, this.player);
   }
 }
